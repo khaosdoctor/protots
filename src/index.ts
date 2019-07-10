435ca55a902ae8bd2336c9cb9cd380e50dbcc59b
Lucas Santos <lhs.santoss@gmail.com>
2019-07-09 21:26:50 -0300

    Updates on parsing
    - Add support for package, generating a namespace for the interfaces
    - Add support for different proto versions, version `proto2` will require the use of `optional` or `required` whereas on version `proto3` all fields are
      optional by default. In short, add support for proto version 2 and 3
    - Removes useless syntax for typescript such as `options`, `syntax` and `package`

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import path from 'path'
 import { promisify } from 'util'
 import { Readable } from 'stream'
 import isPathValid from 'is-valid-path'
+import toCase from 'change-case'
+
+let syntax = 'proto3'
+let hasPackage = false
 const writeAsync = promisify(fs.writeFile)
 
 type ParsedStruct = {
@@ -28,6 +32,62 @@ type ProtobufTypes = {
   'bytes': string
 }
 
+async function readFileStream (stream: Readable): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const chunks: Uint8Array[] = []
+    stream.on('data', (chunk) => chunks.push(chunk))
+    stream.on('error', reject)
+    stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf-8')))
+  })
+}
+
+async function readFile (file: string | Buffer | Readable): Promise<string> {
+  let content = null
+  if (file instanceof Readable) {
+    const stream = await readFileStream(file)
+    return stream
+  }
+  if (typeof file === 'string' && isPathValid(file)) return readFile(fs.createReadStream(path.resolve(file)))
+  if (file instanceof Buffer) content = file.toString('utf-8')
+  if (typeof file === 'string') content = file
+  if (!file || !content) return ''
+
+  return content
+}
+
+function getProtoVersion (contents: string): string | null {
+  const obtainedSyntax = /(?!syntax = ")(proto[0-9])(?=";)/gmi.exec(contents)
+  return obtainedSyntax ? obtainedSyntax.shift() as string : null
+}
+
+function stripUselessSyntax (contents: string): string {
+  return contents.replace(/syntax.*|option.*/gmi, '')
+}
+
+
+function tokenize (line: string): string[] {
+  return line.trim().split(' ').filter(Boolean)
+}
+
+function firstToLower (word: string) {
+  return `${word[0].toLowerCase()}${word.slice(1)}`
+}
+
+function readIndentation (line: string) {
+  const indent = line.length - line.trimLeft().length
+  const indentChar = line[0]
+
+  return { indent, indentChar }
+}
+
+function parseRpcLine (line: string) {
+  const { indent, indentChar } = readIndentation(line)
+  const [, methodWithParams, , responseType] = tokenize(line.replace(/stream /ig, ''))
+  const [methodName, requestType] = methodWithParams.replace(')', '').split('(')
+
+  return `${indentChar.repeat(indent)}${firstToLower(methodName)} (${firstToLower(requestType)}: ${requestType}): ${responseType.replace(/\(|\)/ig, '')}`
+}
+
 function convertToTypescriptTypes (token: keyof ProtobufTypes) {
   const types: ProtobufTypes = {
     'double': 'number',
@@ -50,71 +110,29 @@ function convertToTypescriptTypes (token: keyof ProtobufTypes) {
   return types[token] || token
 }
 
-async function readFileStream (stream: Readable): Promise<string> {
-  return new Promise((resolve, reject) => {
-    const chunks: Uint8Array[] = []
-    stream.on('data', (chunk) => chunks.push(chunk))
-    stream.on('error', reject)
-    stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf-8')))
-  })
-}
-
-async function readFile (file: string | Buffer | Readable): Promise<string[]> {
-  let content = null
-  if (file instanceof Readable) {
-    const stream = await readFileStream(file)
-    return stream.split('\n')
-  }
-  if (typeof file === 'string' && isPathValid(file)) return readFile(fs.createReadStream(path.resolve(file)))
-  if (file instanceof Buffer) content = file.toString('utf-8')
-  if (typeof file === 'string') content = file
-  if (!file || !content) return []
-
-  return content.split('\n')
-}
-
-function tokenize (line: string): string[] {
-  return line.trim().split(' ').filter(Boolean)
-}
-
-function firstToLower (word: string) {
-  return `${word[0].toLowerCase()}${word.slice(1)}`
-}
-
-function readIndentation (line: string) {
-  const indent = line.length - line.trimLeft().length
-  const indentChar = line[0]
-
-  return { indent, indentChar }
-}
-
-function parseRpcLine (line: string) {
-  const { indent, indentChar } = readIndentation(line)
-  const [,methodWithParams,,responseType] = tokenize(line.replace(/stream /ig, ''))
-  const [methodName, requestType] = methodWithParams.replace(')', '').split('(')
-
-  return `${indentChar.repeat(indent)}${firstToLower(methodName)} (${firstToLower(requestType)}: ${requestType}): ${responseType.replace(/\(|\)/ig, '')}`
-}
-
 function parseLine (line: string): string {
   if (!line) return ''
   const { indent, indentChar } = readIndentation(line)
 
   const tokens = tokenize(line)
   let isRepeated = false
-  let isOptional = false
+  let isOptional = syntax === 'proto3' ? true : false
 
   switch (tokens[0]) {
     case '//':
       return line
     case '}':
-      return '}'
+      return `${' '.repeat(hasPackage ? 2 : 0)}}`
     case 'message':
     case 'service':
-      return `interface ${tokens[1]} {`
+      return `${' '.repeat(hasPackage ? 2 : 0)}interface ${tokens[1]} {`
     case 'rpc':
       return parseRpcLine(line)
+    case 'package':
+      hasPackage = true
+      return `namespace ${toCase.pascal(tokens[1].replace(';', ''))} {`
     case 'required':
+      isOptional = false
       tokens.shift()
       break
     case 'optional':
@@ -126,16 +144,21 @@ function parseLine (line: string): string {
       tokens.shift()
   }
 
-  return `${indentChar.repeat(indent)}${tokens[1]}${isOptional ? '?' : ''}: ${convertToTypescriptTypes(tokens[0] as any)}${isRepeated ? '[]' : ''}`
+  return `${indentChar.repeat(indent + (hasPackage ? 2 : 0))}${tokens[1]}${isOptional ? '?' : ''}: ${convertToTypescriptTypes(tokens[0] as any)}${isRepeated ? '[]' : ''}`
 }
 
 async function parse (file: string | Buffer | Readable): Promise<ParsedStruct> {
+  if (!file) throw new Error('No file specified')
   const fileContents = await readFile(file)
-  let parsed = ''
 
-  for (const line of fileContents) {
+  syntax = getProtoVersion(fileContents) || syntax
+
+  let parsed = ''
+  for (const line of stripUselessSyntax(fileContents).split('\n')) {
     parsed += `${parseLine(line)}\n`
   }
+
+  parsed += hasPackage ? '}\n' : ''
 
   return {
     toString: () => parsed,
